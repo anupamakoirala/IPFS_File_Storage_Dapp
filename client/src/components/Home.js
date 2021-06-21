30b40e3a5b47d500a18f44209c8512ed9d5f3046
anupamakoirala <anukoirala.96@hotmail.com>
2021-06-21 13:25:02 +0545

    ipfs-api initialized and IPFS connected with frontend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles,withStyles } from "@material-ui/core/styles";
 import { Paper } from "@material-ui/core";
+import ipfs from "../ipfs";
 import {Button ,Table,TableContainer,TableRow,TableCell,TableBody,TableHead} from "@material-ui/core";
+// import { useState } from "react";
 function Home(){
+    const[buffer,setBuffer]= useState();
+    const[ipfshash,setIpfs] = useState();
     const StyledTableCell = withStyles((theme) => ({
         head: {
           backgroundColor: theme.palette.info.main,
@@ -13,22 +17,48 @@ function Home(){
           fontSize: 14,
         },
       }))(TableCell);
+      //style for table
       const useStyles = makeStyles({
           table:{
               minWidth:400,
           },
       })
       const classes = useStyles();
+//capture and convert file to buffer
+        const handlecapture = (event)=>{
+            event.preventDefault();
+            //capture the userfile 
+            const file = event.target.files[0];
+            //Read the file
+            let reader = new window.FileReader()
+            reader.readAsArrayBuffer(file)
+            //file is converted to a buffer to prepare for uploading to IPFS
+            reader.onloadend=()=>{
+                let buffer = Buffer(reader.result)
+                setBuffer(buffer);
+                console.log("buffer",buffer);
+            }}
+//Save document to IPFS
+const onsubmit = async(event)=>{
+    event.preventDefault();
+    await ipfs.add(buffer,(err,ipfsHash)=>{
+        console.log(ipfsHash)
+        setIpfs(ipfsHash); 
+    })
+}
+
+
+
+        
 
     return(
         <div className="home">
                             <h4> Upload file to add to IPFS</h4>
 
             <div className="upload">
-                <form>
-                    <input type="file"/>
-                    <button bsStyle="primary" type ="submit"
- >Submit</button>
+                <form onSubmit= { onsubmit} >
+                    <input type="file" onChange={handlecapture}/>
+                    <button type ="submit">Submit</button>
                 </form>
                 <br/>
                 <button > Get Transaction receipt</button>
